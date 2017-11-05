398988334902b5b09acc940084f3d96ca3de6afb
BLAH\diana lewakowski <dlewakowski@horizon.csueastbay.edu>
2017-11-04 23:30:37 -0700

    Change Delegate label to Controller

---
+++ CS4310 Fulfillment Program/src/cs4310/fulfillment/program/Controller/EstimatedTimeOfArrival.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs4310.fulfillment.program.Controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Udoka
+ */
+public class EstimatedTimeOfArrival implements Initializable {
+    
+    @FXML Button requestWaitstaff;
+    @FXML Label minutesLeft;
+    String timeLeft;
+    
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //get time left for the order
+    }
+    
+    public void handleRequestWaitstaffButton(ActionEvent event) throws IOException{
+        //send info to the menuorderscene for the waitstaff
+        
+    }
+
+}
---
+++ CS4310 Fulfillment Program/src/cs4310/fulfillment/program/Controller/MenuOrderScene.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs4310.fulfillment.program.Controller;
+
+import cs4310.fulfillment.program.Controller.SceneController;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author jacobh
+ */
+public class MenuOrderScene  implements Initializable {
+    
+    @FXML private Button submitOrder;
+    @FXML private Label totalPrice;
+    @FXML private VBox VBoxQuantity;
+    @FXML private VBox VBoxFood;
+    @FXML private VBox VBoxPrice;
+    
+    @FXML private AnchorPane foodButtonPane;
+    
+    
+    
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //can put top food items here
+    }
+    
+    @FXML public void handleSubmitButton(ActionEvent event) throws IOException{
+        SceneController newScene = new SceneController();
+        newScene.setScene("", submitOrder);
+    }
+    
+    @FXML public void addRowToButtons(){
+        
+    }
+    
+    @FXML public void addColToButton(){
+        
+    }
+    
+    
+    
+}
