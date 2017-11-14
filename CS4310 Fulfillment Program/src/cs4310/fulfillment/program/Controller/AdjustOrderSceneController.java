4f1dac8b60dfa1f86f30dcf4604e729c50c492e5
jimbo408 <coffeehuynh@hotmail.com>
2017-11-13 19:50:53 -0800

    Add files via upload

---
+++ CS4310 Fulfillment Program/src/cs4310/fulfillment/program/Controller/AdjustOrderSceneController.java
@@ -0,0 +1,37 @@
+import java.net.URL;
+import javafx.fxml.FXML;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+
+
+public class AdjustOrderSceneController implements Initializable {
+
+    @FXML private Button orderCompleteButton;
+    @FXML private Button changeTimeButton;
+    @FXML private Button cancelButton;
+    @FXML private ChoiceBox selectTime;
+    
+    @FXML
+    public void handleOrderCompleteButton(ActionEvent e) {
+        
+    }
+    @FXML
+    public void handleChangeTimeButton(ActionEvent e) {
+        
+    }
+    
+    @FXML
+    public void handleCancelButton(ActionEvent e) {
+    Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        selectTime.getItems().addAll(0,5,10,15,20,25,30,35,40,45,50,55,60);
+    }    
+    
+}
