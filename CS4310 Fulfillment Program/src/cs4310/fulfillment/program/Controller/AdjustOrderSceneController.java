43baaa429a9a24bc8c8749cc70df0789cec66156
jimbo408 <coffeehuynh@hotmail.com>
2017-11-13 21:17:34 -0800

    Update AdjustOrderSceneController.java

--- CS4310 Fulfillment Program/src/cs4310/fulfillment/program/Controller/AdjustOrderSceneController.java
+++ CS4310 Fulfillment Program/src/cs4310/fulfillment/program/Controller/AdjustOrderSceneController.java
@@ -4,10 +4,15 @@ import java.net.URL;
 import javafx.fxml.FXML;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.event.ActionEvent;
 import javafx.stage.Stage;
 
+/**
+ *
+ * @author Jimmy
+ */
 
 public class AdjustOrderSceneController implements Initializable {
 
@@ -18,21 +23,25 @@ public class AdjustOrderSceneController implements Initializable {
     
     @FXML
     public void handleOrderCompleteButton(ActionEvent e) {
-        //TODO
+        //return to list of orders, remove order from list of orders
+        
+        newScene.setScene("/cs4310/fulfillment/program/View/ListofOrdersScene.fxml", (Button)e.getSource());
     }
     @FXML
     public void handleChangeTimeButton(ActionEvent e) {
-        //TODO
+        //return to list of orders, change remaining wait time
+        
+        newScene.setScene("/cs4310/fulfillment/program/View/ListofOrdersScene.fxml", (Button)e.getSource());
     }
     
     @FXML
     public void handleCancelButton(ActionEvent e) {
-    Stage stage = (Stage) cancelButton.getScene().getWindow();
-        stage.close();
+        newScene.setScene("/cs4310/fulfillment/program/View/ListofOrdersScene.fxml", (Button)e.getSource());
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        newScene = new SceneController();
         selectTime.getItems().addAll(0,5,10,15,20,25,30,35,40,45,50,55,60);
     }    
     
