13b70dddc7484d19e503af11af7b35e308dbb86d
CJVincent <christophvincent@gmail.com>
2017-11-17 15:40:15 -0800

    Add EditSelectUserSceneController

--- CS4310 Fulfillment Program/src/cs4310/fulfillment/program/Controller/UserLoginSceneController.java
+++ CS4310 Fulfillment Program/src/cs4310/fulfillment/program/Controller/UserLoginSceneController.java
@@ -14,6 +14,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 /**
@@ -27,7 +28,7 @@ public class UserLoginSceneController implements Initializable {
     @FXML private Button loginButton;
     @FXML private ChoiceBox<String> roleSelectBox;
     @FXML private TextField usernameField;
-    @FXML private TextField passwordField;
+    @FXML private PasswordField passwordField;
     @FXML private Label usernameLabel;
     @FXML private Label passwordLabel;
     @FXML private Label roleLabel;
