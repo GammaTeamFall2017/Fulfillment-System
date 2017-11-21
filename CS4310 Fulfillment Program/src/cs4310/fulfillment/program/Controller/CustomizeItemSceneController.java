95d4473c531cf813a1a5a4d877180510f8087a97
jimbo408 <coffeehuynh@hotmail.com>
2017-11-21 15:21:34 -0800

    Update CustomizeItemSceneController.java

--- CS4310 Fulfillment Program/src/cs4310/fulfillment/program/Controller/CustomizeItemSceneController.java
+++ CS4310 Fulfillment Program/src/cs4310/fulfillment/program/Controller/CustomizeItemSceneController.java
@@ -6,6 +6,8 @@
 package cs4310.fulfillment.program.Controller;
 
 import cs4310.fulfillment.program.Controller.SceneController;
+import cs4310.fulfillment.program.Model.DbUtilityCollection;
+import cs4310.fulfillment.program.Model.Subitem;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -15,6 +17,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.control.CheckBox;
 
 /**
  * FXML Controller class
@@ -42,17 +45,39 @@ public class CustomizeItemSceneController implements Initializable {
     @FXML
     private Button addButton;
     
+    private Subitem sItem;
     private SceneController newScene;
+    
+    DbUtilityCollection db = new DbUtilityCollection();
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+       
         newScene = new SceneController();
+        sItem = new Subitem();
+        String sInstruction = specialInstructionsField.getText();
+        int quantity = Integer.parseInt(quantityField.getText());
+        
         //disable submitButton if quantity field is empty; avoid empty orders
         submitButton.disableProperty().bind(quantityField.textProperty().isEmpty());
+        
+        CheckBox cb = new CheckBox();
+        int totalSubitems = 5;
+        VBox vb[] = new VBox[totalSubitems];
+        for (int j = 0; j < totalSubitems; j++) {
+            cb = new CheckBox("Item: " + db.getSubitemByID(j)); 
+            vb[j] = new VBox();
+            vb[j].setStyle("-fx-border-style: solid;"
+                    + "-fx-border-width: 1;"
+                    + "-fx-min-height: 500px;"
+                    + "-fx-max-width: 200px;"
+                    + "-fx-border-color: black");
+            vb[j].getChildren().add(cb);
+        }
+        boolean isSelected = cb.isSelected();
     }    
 
     @FXML
@@ -62,6 +87,8 @@ public class CustomizeItemSceneController implements Initializable {
 
     @FXML
     private void handleSubmitButton(ActionEvent event) {
+        
+        newScene.setScene("/cs4310/fulfillment/program/View/MenuOrderScene.fxml", (Button)event.getSource());
     }
 
     @FXML
