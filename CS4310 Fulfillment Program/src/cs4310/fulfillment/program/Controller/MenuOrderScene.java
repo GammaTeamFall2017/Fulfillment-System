cd7f8154a5299fead4a8f3f1deec8f180523ed12
BLAH\diana lewakowski <dlewakowski@horizon.csueastbay.edu>
2017-11-20 20:40:06 -0800

    Added code to created line item entries in the database for each ItemsOrdered object in orderArray. tempOrderArray contains similar objects to orderArray, but includes their newly created id keys.

--- CS4310 Fulfillment Program/src/cs4310/fulfillment/program/Controller/MenuOrderScene.java
+++ CS4310 Fulfillment Program/src/cs4310/fulfillment/program/Controller/MenuOrderScene.java
@@ -14,7 +14,9 @@ import java.math.BigDecimal;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -221,9 +223,21 @@ public class MenuOrderScene  implements Initializable {
         if(CS4310FulfillmentProgram.getCurrentUserRole().equals("waitstaff")){
             
         }
-        submitOrder.setText("Recieved");
+        submitOrder.setText("Received");
         receivedOrder = true;
-        newOrder.setItemsOrderedCollection(orderArray);
+        
+        // Create line item entries in the database
+        List<ItemsOrdered> tempOrderArray = new ArrayList<ItemsOrdered>(); // Store the ItemsOrdered object created in database to new list with their new id
+        // If these are the items we want in the order, first add each line item as entries into the database to ItemsOrdered 
+        for (Iterator<ItemsOrdered> i = orderArray.iterator(); i.hasNext();) {
+            ItemsOrdered lineItem =  i.next();
+            
+            ItemsOrdered tempItemsOrdered = DatabaseConnecter.createNewItemsOrdered(lineItem);
+            tempOrderArray.add(tempItemsOrdered);
+        }
+        
+        // Attach the tempOrderArray to our order since it contains the ids needed to associated the new order with its line items in the database 
+        newOrder.setItemsOrderedCollection(tempOrderArray);
         
         Calendar cal = Calendar.getInstance();
         Date currentDate = new Date(cal.getTimeInMillis());
